342465167eac3106a2a7f1fa4169ed5465071125
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-03-06 23:45:22 -0300

    Structuring instructors data, searching them with route parameter and structuring the instructors view

--- instructors.js
+++ instructors.js
@@ -1,6 +1,18 @@
 const fs = require('fs')
 const data = require('./data.json')
-// create
+
+exports.show = function(req, res) {
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+
+    if(!foundInstructor) return res.send("Instructor not found")
+
+    return res.render("instructors/show", { instructor: foundInstructor })
+}
+
 exports.post = function(req, res) {
     const keys = Object.keys(req.body)
 
@@ -10,7 +22,21 @@ exports.post = function(req, res) {
         }
     }
 
-    data.instructors.push(req.body)
+    let { avatar_url, name, birth, gender, services } = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    data.instructors.push({
+        id, 
+        avatar_url,
+        name, 
+        birth, 
+        gender,
+        services, 
+        created_at 
+    })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
         if(err) return res.send("Write file error")
