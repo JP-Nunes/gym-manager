5b8527e6a55b27f9d072f0ef2c2feaa4717bd8dc
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-03-12 19:03:44 -0300

    Exporting with shorthand method, configuring database connection and inseting data at the PostgreSQL database

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,118 +1,76 @@
-const fs = require('fs')
-const data = require('../data.json')
-const { date } = require('../utils')
-const intl = require('intl')
+const { age, date } = require('../../lib/utils')
 
-exports.index = function(req, res) {
-    return res.render("members/index", { members: data.members })
-}
-
-exports.create = function(req, res) {
-    return res.render("members/create")
-}
-
-exports.show = function(req, res) {
-    const { id } = req.params
-
-    const foundMember = data.members.find(function(member){
-        return member.id == id
-    })
-    if(!foundMember) return res.send("Member not found")
+module.exports = {
+    index(req, res) {
+        return res.render("members/index", { members })
+    },
     
-    const member = {
-        ...foundMember,
-        birth: date(foundMember.birth).birthDay
-    }
+    create(req, res) {
+        return res.render("members/create")
+    },
 
-    return res.render("members/show", { member })
-}
-
-exports.edit = function(req, res) {
-    const { id } = req.params
-
-    const foundMember = data.members.find(function(member){
-        return member.id == id
-    })
-    if(!foundMember) return res.send("Member not found")
-
-    const member = {
-        ...foundMember,
-        birth: date(foundMember.birth).iso
+    post(req, res) {
+        const keys = Object.keys(req.body)
+    
+        for(key of keys) {
+            if(req.body[key] == "") {
+                return res.send("Please fill all required fields")
+            }
+        }
+    },
+    
+    show(req, res) {
+        const { id } = req.params
+    
+        const foundmember = data.members.find(function(member){
+            return member.id == id
+        })
+        if(!foundmember) return res.send("member not found")
         
-    }
-
-    return res.render('members/edit', { member })
-}
-
-exports.post = function(req, res) {
-    const keys = Object.keys(req.body)
-
-    for(key of keys) {
-        if(req.body[key] == "") {
-            return res.send("Please fill all required fields")
+        const member = {
+            ...foundmember,
+            age: age(foundmember.birth),
+            services: foundmember.services.split(","),
+            created_at: new intl.DateTimeFormat("pt-BR").format(foundmember.created_at)
         }
-    }
-
-    birth = Date.parse(req.body.birth)
     
-    let id = 1
-    const lastMember = data.members[data.members.length - 1]
-
-    if(lastMember) {
-        id = lastMember.id + 1
-    } 
-
-    data.members.push({
-        id,
-        birth,
-        ...req.body
-    })
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if(err) return res.send("Write file error")
-
-        return res.redirect(`/members/${id}`)
-    })
-}
-
-exports.delete = function(req, res) {
-    const { id } = req.body
-
-    const filteredMembers = data.members.filter(function(member) {
-        return member.id != id
-    })
-    data.members = filteredMembers
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if (err) return res.send("member not found.")
-
-        return res.redirect("/members")
-    })
-}
-
-exports.put = function(req, res) {
-const { id } = req.body
-    let index = 0
-
-    const foundMember = data.members.find(function(member, foundIndex) {
-        if(id == member.id) {
-            index = foundIndex
+        return res.render("members/show", { member })
+    },
+    
+    edit(req, res) {
+        const { id } = req.params
+    
+        const foundmember = data.members.find(function(member){
+            return member.id == id
+        })
+        if(!foundmember) return res.send("member not found")
+    
+        const member = {
+            ...foundmember,
+            birth: date(foundmember.birth).iso
+            
         }
-    })
-
-
-    const member = {
-        ...foundMember,
-        ...req.body,
-        birth: Date.parse(req.body.birth),
-        id: Number(req.body.id)
+    
+        return res.render('members/edit', { member })
+    },
+    
+    put(req, res) {
+        const { id } = req.body
+        let index = 0
+    
+        const foundmember = data.members.find(function(member, foundIndex) {
+            if(id == member.id) {
+                index = foundIndex
+            }
+        })
+    },
+    
+    delete(req, res) {
+        const { id } = req.body
+    
+        const filteredmembers = data.members.filter(function(member) {
+            return member.id != id
+        })
+        data.members = filteredmembers
     }
-
-    data.members[index] = member
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if(err) return res.send("Write error")
-
-        return res.redirect(`/members/${member.id}`)
-    })
 }
