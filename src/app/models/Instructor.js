1dd1fe800a56d512edad089aad6222a96b12116a
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-03-17 22:28:56 -0300

    Implementing filter logic

--- src/app/models/Instructor.js
+++ src/app/models/Instructor.js
@@ -3,7 +3,8 @@ const { age, date } = require('../../lib/utils')
 
 module.exports = {
     all(callback) {
-        db.query(`SELECT instructors.*, count(members) AS total_students
+        db.query(
+        `SELECT instructors.*, count(members) AS total_students
         FROM instructors
         LEFT JOIN members ON (members.instructor_id = instructors.id)
         GROUP BY instructors.id
@@ -49,6 +50,21 @@ module.exports = {
                 callback(results.rows[0])
             }
         )
+    },
+
+    findBy(filter, callback) {
+        db.query(
+        `SELECT instructors.*, count(members) AS total_students
+        FROM instructors
+        LEFT JOIN members ON (members.instructor_id = instructors.id)
+        WHERE instructors.name ILIKE '%${filter}%'
+        OR instructors.services ILIKE '%${filter}%'
+        GROUP BY instructors.id
+        ORDER BY total_students DESC`, function(err, results) {
+            if(err) throw `Database Error! ${err}`
+    
+            callback(results.rows)
+        })
     },
 
     update(data, callback) {
